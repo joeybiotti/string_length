5b9b6228d27dcf7ff435b7e988f3af1c4d6baaf8
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-11 22:20:32 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace string_big_or_small
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+           
+            string line = Console.ReadLine();
+           
+            int value;
+            Int32.TryParse(line, out value);
+
+            if(value < 10)
+            {
+                Console.WriteLine("This number is too big");
+            }
+            else
+            {
+                Console.WriteLine("This number is big enough");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Interactive Console App
+
+Console Kata. 
+
+Dotnet run. Enter a number. If its more less than 10 get one response. If its more, get the other. Real simple. 
+
+The purpose of this is to get more practice setting up C# projects. 
